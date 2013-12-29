8286af23722a90901a46e2c8a8a56463bf5a49f9
grafts <minwoo@grafts.tv>
2013-12-29 19:55:19 +0000

    add font plugin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,27 +21,35 @@ require.config({
 		}
 	},
 	paths: {
-		router : '../scripts/routes/routes',
-		jquery: '../bower_components/jquery/jquery',
-		backbone: '../bower_components/backbone/backbone',
-		backboneController: '../scripts/vendor/backbone.controller',
+		router             : '../scripts/routes/routes',
+		jquery             : '../bower_components/jquery/jquery',
+		backbone           : '../bower_components/backbone/backbone',
+		backboneController : '../scripts/vendor/backbone.controller',
 		// replace underscore >> lodash
-		underscore: '../bower_components/lodash/dist/lodash',
-		handlebars: '../bower_components/handlebars/handlebars',
-
-		loading : '../scripts/views/loading',
-
-		editor: '../scripts/vendor/medium'
+		underscore         : '../bower_components/lodash/dist/lodash',
+		handlebars         : '../bower_components/handlebars/handlebars',
+		
+		font               : '../bower_components/requirejs-plugins/src/font',
+		propertyParser     : '../bower_components/requirejs-plugins/src/propertyParser',
+		
+		loading            : '../scripts/views/loading',
+		
+		editor             : '../scripts/vendor/medium'
 	}
 });
 
 require([
-	'backbone', 'backboneController', 'router'
+	'backbone', 'backboneController', 'router', 'propertyParser'
 ], function (Backbone, Controller, Router) {
 	var router,
 		pushState = !!window.history.pushState;
 
 	router = new Router();
 	Backbone.history.start({ hashChange: !pushState, pushState : pushState });
+
+	require(['font!custom,families:[Nanum Gothic, Nanum Myeongjo],urls:[//fonts.googleapis.com/earlyaccess/nanumgothic.css, //fonts.googleapis.com/earlyaccess/nanummyeongjo.css]'], function(){
+		$('.serif').css('font-family', 'Nanum Gothic');
+		$('.sans-serif').css('font-family', 'Nanum Myeongjo');
+	});
 
 });
