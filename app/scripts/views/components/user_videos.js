3bcd8d3a5757e0f40b0b6c51c70b282109e21d8f
grafts <minwoo@grafts.tv>
2014-04-05 16:53:35 +0900

    draft documents presentation

--- app/scripts/views/components/user_videos.js
+++ app/scripts/views/components/user_videos.js
@@ -11,6 +11,7 @@ define([
 		tagName : 'div',
 		template : JST['app/scripts/templates/user_videos.hbs'],
 		events : {
+			'click a' : 'link'
 		},
 		initialize : function(data){
 			this.userId    = data.userId;
@@ -24,10 +25,28 @@ define([
 			return this.$el;
 		},
 		updateDom : function(){
-			var self = this;
+			var self = this,
+				videos = this.collection.JSON(),
+				published, drafts;
+
+
+			published = videos.filter(function(video){
+				return video.publish == true;
+			});
+			drafts = videos.filter(function(video){
+				return !video.publish;
+			});
+			videos = {
+				published : published
+			};
+
+			if(this.getEditAuth()){
+				videos.drafts = drafts;
+			}
+
 			this.empty();
 			this.$el
-			.append(self.template({ videos : self.collection.JSON() }));
+			.append(self.template(videos));
 		},
 		empty : function(){
 			this.$el.empty();
@@ -43,8 +62,18 @@ define([
 		add : function(model){
 			this.collection.add(model);
 		},
+		getEditAuth : function(){
+			if(this.userId == Backbone.User.current().id){
+				return true;
+			}
+			else {
+				return false;
+			}
+		},
 		link : function(e){
-			
+			e.preventDefault();
+			e.stopPropagation();
+			Backbone.history.navigate(e.target.pathname || e.target.parentNode.pathname || e.target.parentNode.parentNode.pathname, { trigger : true });
 		}
 	});
 
